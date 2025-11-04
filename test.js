03b010c096e47e50c129f12d7bc6da4b038066e4
Miocarde <houdini.tp@gmail.com>
2025-11-04 17:10:52 +0100

    added some vulns

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
-function endsWith(x,y) {
-
-
-    let index = x.lastIndexOf(y);
+function endsWith(x, y) {
+    if (typeof x !== 'string' || typeof y !== 'string') {
+        throw new TypeError('Both arguments must be strings');
+    }
+    if (y === '') return true;
     return x.lastIndexOf(y) === x.length - y.length;
 }
+
+module.exports = endsWith;
